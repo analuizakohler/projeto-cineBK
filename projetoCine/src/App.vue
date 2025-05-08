20964ded34bdc2becd52deb64f01e59ea88f0db1
analuizakohler <analuizakohler24@gmail.com>
2025-05-08 11:03:39 -0300

    FEAT: template filmes #2

--- projetoCine/src/App.vue
+++ projetoCine/src/App.vue
@@ -23,7 +23,25 @@
 <section v-if="mostrarCarrinho" class="carrinho">
   <h2>Carrinho de Compras</h2>
   <div v-if="carrinho.items.length === 0">
-    <p>Carrinho Vazio</p>
+    <p>Carrinho Vazio.</p>
+  </div>
+  <div v-else>
+    <div v-for="item in carrinho.items" :key="item.id" class="item-carrinho">
+      <div class="info">
+        <strong>{{ item.nome }}</strong>
+        <p>R${{ item.preco.toFixed(2) }}</p>
+        <div class="quantidade">
+          <button @click="alterarQuantidade(item,-1)">-</button>
+          <span>{{ item.quantidade }}</span>
+          <button @click="alterarQuantidade(item, 1)">+</button>
+        </div>
+        <p>Total: R${{ item.valorTotal.toFixed(2) }}</p>
+      </div>
+        <button class="remover" @click="removerItem(item)">remover</button>
+    </div>
+    <div class="total">
+      <h3>Total: R${{ carrinho.total.toFixed(2) }}</h3>
+    </div>
   </div>
 </section>
 </template>
